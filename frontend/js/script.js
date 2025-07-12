8978adae85d80689503e589b3677f45ec596e600
kuldeep <kuldeephawladar14@gmail.com>
2025-07-12 09:38:16 +0530

    quiz page created

---
+++ frontend/js/script.js
@@ -0,0 +1,5 @@
+// js/ for quiz 
+document.getElementById("careerQuizForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Form submitted! (backend integration coming soon)");
+});
